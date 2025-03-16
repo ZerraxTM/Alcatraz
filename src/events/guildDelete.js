406451b96e6ece0233026755663860408a2298ce
!ᴢᴇʀʀᴀx™ <119348577+ZerraxTM@users.noreply.github.com>
2025-03-16 18:04:13 +0100

    Commandes +220

---
+++ src/events/guildDelete.js
@@ -0,0 +1,31 @@
+//  ______   __                      __                                  
+// /      \ |  \                    |  \                                 
+//|  $$$$$$\| $$  _______  ______  _| $$_     ______   ______   ________ 
+//| $$__| $$| $$ /       \|      \|   $$ \   /      \ |      \ |        \
+//| $$    $$| $$|  $$$$$$$ \$$$$$$\\$$$$$$  |  $$$$$$\ \$$$$$$\ \$$$$$$$$
+//| $$$$$$$$| $$| $$      /      $$ | $$ __ | $$   \$$/      $$  /    $$ 
+//| $$  | $$| $$| $$_____|  $$$$$$$ | $$|  \| $$     |  $$$$$$$ /  $$$$_ 
+//| $$  | $$| $$ \$$     \\$$    $$  \$$  $$| $$      \$$    $$|  $$    \
+// \$$   \$$ \$$  \$$$$$$$ \$$$$$$$   \$$$$  \$$       \$$$$$$$ \$$$$$$$$
+//=======================================================================                                                                      
+//● Crée par GalackQSM le 09 novembre 2019 Update and cover by LeZ in 2025
+//● Serveur Discord: https://discord.gg/zch
+//● Github: https://github.com/GalackQSM/Alcatraz                                                      
+//=======================================================================                                                                      
+
+const { MessageEmbed } = require('discord.js');
+const { fail } = require('../utils/emojis.json');
+
+module.exports = (client, guild) => {
+
+  client.logger.info(`Alcatraz viens de quitté le serveur:  ${guild.name}`);
+  const serverLog = client.channels.cache.get(client.serverLogId);
+  if (serverLog)
+    serverLog.send(new MessageEmbed().setDescription(`${client.user} viens de quitté le serveur: **${guild.name}** ${fail}`));
+
+  client.db.settings.deleteGuild.run(guild.id);
+  client.db.users.deleteGuild.run(guild.id);
+
+  if (guild.job) guild.job.cancel(); 
+
+};
---
+++ src/events/guildUpdate.js
@@ -0,0 +1,24 @@
+//  ______   __                      __                                  
+// /      \ |  \                    |  \                                 
+//|  $$$$$$\| $$  _______  ______  _| $$_     ______   ______   ________ 
+//| $$__| $$| $$ /       \|      \|   $$ \   /      \ |      \ |        \
+//| $$    $$| $$|  $$$$$$$ \$$$$$$\\$$$$$$  |  $$$$$$\ \$$$$$$\ \$$$$$$$$
+//| $$$$$$$$| $$| $$      /      $$ | $$ __ | $$   \$$/      $$  /    $$ 
+//| $$  | $$| $$| $$_____|  $$$$$$$ | $$|  \| $$     |  $$$$$$$ /  $$$$_ 
+//| $$  | $$| $$ \$$     \\$$    $$  \$$  $$| $$      \$$    $$|  $$    \
+// \$$   \$$ \$$  \$$$$$$$ \$$$$$$$   \$$$$  \$$       \$$$$$$$ \$$$$$$$$
+//=======================================================================                                                                      
+//● Crée par GalackQSM le 09 novembre 2019 Update and cover by LeZ in 2025
+//● Serveur Discord: https://discord.gg/zch
+//● Github: https://github.com/GalackQSM/Alcatraz                                                  
+//=======================================================================                                                                      
+                                                                       
+module.exports = (client, oldGuild, newGuild) => {
+  
+  if (oldGuild.name == newGuild.name) return;
+  
+  client.db.settings.updateGuildName.run(newGuild.name, oldGuild.id);
+  client.db.users.updateGuildName.run(newGuild.name, oldGuild.id);
+
+  client.logger.info(`${oldGuild.name} le nom du serveur est devenu ${newGuild.name}`);
+};
